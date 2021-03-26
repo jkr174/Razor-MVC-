08decb90f4cc1ae9bd006e041d8aabed7c9b26a5
jkr174 <romo17410@gmail.com>
2021-03-25 19:41:33 -0500

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Razor_MVC_.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor_MVC_.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult index()
+        {
+            Product[] array =
+            {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price= 48.95M},
+                new Product {Name = "Soccer ball", Price = 19.50M},
+                new Product {Name = "Corner flag", Price = 34.95M}
+            };
+
+            return View(array);
+        }
+    }
+}
